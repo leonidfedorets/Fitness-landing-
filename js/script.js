e2b6b8e45ba15595964f7b3ece7d9def70830fe9
leo <leonidfedorets30@gmai.com>
2022-01-27 01:34:34 +0200

    Modal init

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
     //Timer
-    const deadline = '2020-05-11';
+    const deadline = '2022-05-11';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -93,15 +93,39 @@ window.addEventListener('DOMContentLoaded', function() {
     setClock('.timer', deadline);
 
 
+//Modal
 
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+modal = document.querySelector('.modal'),
+modalCloseBtn = document.querySelector('[data-close]');
 
+modalTrigger.forEach(btn => {
+btn.addEventListener('click', function() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    // Либо вариант с toggle - но тогда назначить класс в верстке
+    document.body.style.overflow = 'hidden';
+});
+});
 
+function closeModal() {
+modal.classList.add('hide');
+modal.classList.remove('show');
+// Либо вариант с toggle - но тогда назначить класс в верстке
+document.body.style.overflow = '';
+}
 
+modalCloseBtn.addEventListener('click', closeModal);
 
+modal.addEventListener('click', (e) => {
+if (e.target === modal) {
+    closeModal();
+}
+});
 
-
-
-
-
-    
+document.addEventListener('keydown', (e) => {
+if (e.code === "Escape" && modal.classList.contains('show')) { 
+    closeModal();
+}
+});
 });
