9b25b96ffb1a3bf97cfebfdbe569013e15be419d
leo <leonidfedorets30@gmai.com>
2022-02-12 14:31:16 +0200

    add tab content

--- RealLife/assets/js/tabs.js
+++ RealLife/assets/js/tabs.js
@@ -1,9 +1,16 @@
  // Tabs
     
  let tabs = document.querySelectorAll('.nav-item'),
- tabsParent = document.querySelector('.nav-header');
+ tabsParent = document.querySelector('.nav-header'),
+ tabsContent = document.querySelectorAll('.tabcontent');
 
  function hideTabContent() {
+        
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
         
     tabs.forEach(item => {
         item.classList.remove('current');
@@ -11,7 +18,8 @@
 }
 
 function showTabContent(i = 0) {
-    
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('current');
 }
 
